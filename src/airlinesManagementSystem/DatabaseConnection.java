1b5cca2a3a55c8797f3351113a8f45d66b5ee944
Rakibur Khan <therakiburkhan@outlook.com>
2020-03-29 04:57:41 +0600

    Basic Structure Established

---
+++ src/airlinesManagementSystem/DatabaseConnection.java
@@ -0,0 +1,40 @@
+package airlinesManagementSystem;
+
+/**
+ *
+ * @author MD Rakibur Khan
+ */
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class DatabaseConnection {
+    
+    public static Connection Connect(){
+        try {
+            //Checking JDBC drivar availibility
+            Class.forName("org.sqlite.JDBC");
+            
+        } catch (ClassNotFoundException classNotFoundException) {
+            
+            JOptionPane.showMessageDialog(null, classNotFoundException, "Database Driver Error!!!", JOptionPane.ERROR_MESSAGE);
+        
+        }
+        
+        try {
+            // database parameters
+            String url = "jdbc:sqlite:src/assets/database/Airlines Management System.sqlite3";
+            // create a connection to the database
+           Connection connection = DriverManager.getConnection(url);
+  
+            return connection;
+            
+        } catch (SQLException sQLException) {
+            
+            JOptionPane.showMessageDialog(null, sQLException, "SQL Error!!!", JOptionPane.ERROR_MESSAGE);
+            
+            return null;
+        }
+    }
+}
+ 
---
+++ src/airlinesManagementSystem/MainClass.java
@@ -0,0 +1,17 @@
+package airlinesManagementSystem;
+
+/**
+ *
+ * @author MD Rakibur Khan
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+    
+}
